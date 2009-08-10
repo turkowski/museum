2bc55969436a949791581e52fda367f7ebc098bf
danx0r <danbmil99@gmail.com>
2009-08-10 00:08:32 -0700

    updated upload.py to use ftp instead of scp

--- upload.py
+++ upload.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python
 import os, sys
-
-print "upload.py -- upload content to sirikata website"
-print "this software is released under the BSD open source license.  See sirikata.com for more details"
+from ftplib import FTP
 
 WINDOWS = False             #cygwin is not windows!
-SCP = "scp"
 
 ## shedskin windows works in console, not cygwin!
 
 if "win32" in sys.platform or "shedskin" in sys.platform:
     WINDOWS = True
-    SCP = "pscp -i id_rsa_private.ppk"
 
 nulldev = "NUL" if WINDOWS else "/dev/null"
 
@@ -25,7 +21,6 @@ def system(s):
     return os.system(fixsysline(s))
 
 def checkhttpfile(path):
-    ## return file length or zero for not on server
     cmd = "wget -S --spider -o " + nulldev + " " + path
     ##print "os.cmd:", cmd
     err = os.system(cmd)
@@ -35,20 +30,17 @@ def checkhttpfile(path):
 
 hashes = set()
 def main():
-    print "creating or clearing temporary directory"
-    if "tempSirikataUpload" in os.listdir("."):
+    try:
         if not os.listdir("tempSirikataUpload") == []:
             if WINDOWS:
                 os.system("del /Q tempSirikataUpload\*")
             else:
                 os.system("rm tempSirikataUpload/*")
-    else:
+    except:
         system("mkdir tempSirikataUpload")
-    print "creating name files:"
     f = open("Staging/names.txt")
     for i in f.readlines():
         name, hash = i.strip().split()
-        print name
         clean = hash[-64:]
         hashes.add(clean)
         fo = open("tempSirikataUpload/"+name, "w")
@@ -56,10 +48,20 @@ def main():
         fo.close()
     f.close()
 
+    print "----------------------------------------------------------"
     print "copying name files:"
-    cmd = SCP + fixsysline(" tempSirikataUpload/*") + " henrikbennetsen@delorean.dreamhost.com:sirikata.com/content/names/."
-    ##print "os.cmd:", cmd
-    os.system(cmd)
+##    cmd = SCP + fixsysline(" tempSirikataUpload/*") + " henrikbennetsen@delorean.dreamhost.com:sirikata.com/content/names/."
+##    ##print "os.cmd:", cmd
+##    os.system(cmd)
+    ftp = FTP("sirikata.com")
+    ftp.login("slartist", "V3sb4Dkb")
+    files = os.listdir(fixsysline("tempSirikataUpload"))
+    for fil in files:
+        print "copying ", fil
+        cmd = "STOR content/names/" + fil
+        f = open(fixsysline("tempSirikataUpload/")+fil)
+        ftp.storbinary(cmd, f)
+        f.close()
     print "done"
     print "----------------------------------------------------------"
     print "copying asset files:"
@@ -70,11 +72,17 @@ def main():
             if checkhttpfile("http://www.sirikata.com/content/assets/" + i):
                 print i, "found on server, not copying"
             else:
+                print "copying", i
                 cachedir = "Staging/" if (i in assetlist2) else "Cache/"
-                cmd = fixsysline(SCP+" " + cachedir + i) + " henrikbennetsen@delorean.dreamhost.com:sirikata.com/content/assets/."
-                ##print "os.cmd:", cmd
-                os.system(cmd)
+##                cmd = fixsysline(SCP+" " + cachedir + i) + " henrikbennetsen@delorean.dreamhost.com:sirikata.com/content/assets/."
+##                ##print "os.cmd:", cmd
+##                os.system(cmd)
+                cmd = "STOR content/assets/" + i
+                f = open(fixsysline(cachedir)+i)
+                ftp.storbinary(cmd, f)
+                f.close()
 
+    ftp.quit()
     print "done"
 
 main()
